f3b2f85297aa0e2bf2eeefe072c95173f2564ffd
Alexis Herrera <alexizher@gmail.com>
2025-10-06 23:57:47 -0500

    Creación de la estructura del proyecto

---
+++ onboarding-pymes/src/main/java/tech/nocountry/onboarding/OnboardingPymesApplication.java
@@ -0,0 +1,13 @@
+package tech.nocountry.onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingPymesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingPymesApplication.class, args);
+	}
+
+}
